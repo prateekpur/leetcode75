e50f385aeac8ecfa99434730a49571b497009622
Prateek Puri <prateekpuri@yahoo.com>
2024-12-18 12:27:37 -0500

    Problem https://leetcode.com/problems/decode-string/description/?envType=study-plan-v2&envId=leetcode-75

--- my-leetcode/src/main/java/leetcode/StringEncoder.java
+++ my-leetcode/src/main/java/leetcode/StringEncoder.java
@@ -63,108 +63,7 @@ public class StringEncoder {
 		//logger.log(Level.INFO, prefix.toString());
 		return decodeString3(prefix.toString());
 	}
-	
-	public static void decodeString2(String s)	{
-		int tmp = 0, cntOpeningBrackets=0;
-		boolean openingBracket = false;
-		StringBuilder prefix = new StringBuilder(), withinBracket = new StringBuilder();
-		for (int i = 0; i < s.length(); ++i)	{
-			if(openingBracket)	{
-				withinBracket.append(s.charAt(i));
-			}
-			if (!openingBracket && intSet.contains(s.charAt(i)))	{
-				tmp *= 10;
-				tmp += Integer.parseInt(Character.toString(s.charAt(i)));
-			}
-			if (s.charAt(i) == '[')	{
-				++cntOpeningBrackets;
-				openingBracket = true;
-			}
-			else if (s.charAt(i) == ']')	{
-				--cntOpeningBrackets;
-				if (cntOpeningBrackets == 0)	{
-					//remove last character from withinBracket
-					withinBracket.deleteCharAt(withinBracket.lastIndexOf("]"));
-					logger.log(Level.INFO, "prefix : " + prefix.toString() + " : count : " + Integer.toString(tmp) + " : between brackets : " + withinBracket.toString());
-					return;
-				}
-			}
-		}
-	}
-	
-	public static String decodeString1(String s, int index) {
-		StringBuilder bld = new StringBuilder("");
-		Stack<Character> stack = new Stack();
-		Set<Character> intSet = new HashSet();
-		int tmp = 0, openingBrackets=0;
-		intSet.add('0');intSet.add('1');intSet.add('2');intSet.add('3');intSet.add('4');intSet.add('5');intSet.add('6');intSet.add('7');intSet.add('8');intSet.add('9');
-		for (int i = index; i < s.length(); ++i)	{
-			char c = s.charAt(i);
-			if (tmp != 0)	{
-				logger.log(Level.INFO, " : " + tmp);
-			}
-			if (c == ']')	{
-				if (openingBrackets == 0)	{
-					return bld.toString();
-				}
-				--openingBrackets;
-				//find string till ']'
-			}	else if (c == '[')	{
-				++openingBrackets;
-				String s1 = decodeString1(s, ++i);
-				logger.info("String : " + s1);
-				bld.append(s1);
-			}	else if (intSet.contains(c)){
-				tmp *= 10;
-				tmp += Integer.parseInt(Character.toString(c));
-			}	else	{
-				bld.append(c);
-			}
-		}
-		return "";
-	}
-	
-	public static String expandString(String s, int index)	{
-		StringBuilder bld = new StringBuilder("");
-		int cntOpen = 1;
-		for (int i = index; i < s.length() && cntOpen != 0; ++i)	{
-			if (s.charAt(i) == '[')	{
-				++cntOpen;
-				bld.append(expandString(s, ++index));
-			}	else if (s.charAt(i) == ']') {
-				--cntOpen;
-			}	else	{
-				bld.append(s.charAt(i));
-			}
-		}
-		return bld.toString();
-	}
-	
-	public static String decodeString(String s) {
-		StringBuilder bld = new StringBuilder("");
-		Set<Character> intSet = new HashSet();
-		intSet.add('0');intSet.add('1');intSet.add('2');intSet.add('3');intSet.add('4');intSet.add('5');intSet.add('6');intSet.add('7');intSet.add('8');intSet.add('9');
-		int tmp = 0;
-		boolean startSquare = false;
-		for (char c: s.toCharArray())	{
-			if(intSet.contains(c))	{
-				tmp *= 10;
-				tmp += Integer.parseInt(Character.toString(c));
-			}	else if (c == '[')	{
-				startSquare = true;
-			}	else if (c == ']')	{
-				logger.log(Level.INFO, bld.toString() + " : " + Integer.toString(tmp));
-				bld.setLength(0);
-				tmp = 0;
-				startSquare = false;
-			}	else	{
-				bld.append(c);
-			}
-		}
-		logger.log(Level.INFO, bld.toString());
-		
-		return "";
-	}
+
 	
 	public static String expand(String s, int startIndex)	{
 		int cntOpeningBrackets = 1, index = startIndex, secondBracketIndex=-1;
@@ -188,17 +87,6 @@ public class StringEncoder {
 		}
 		return "";
 	}
-    /*public static String decodeString(String s) {
-    	logger.setLevel(Level.INFO);
-    	int nidex = 0;
-    	while (s.indexOf(']') != -1)	{
-    		int i = s.indexOf('[');
-    		String s1 = s.substring(i+1, s.indexOf(']'));
-    		logger.log(Level.INFO, s1);
-    	}
-    	int i = s.indexOf(']');
-    	return "";
-    }*/
     
     public static int getNum(int index)	{
 		logger.log(Level.INFO, " : "+index);
